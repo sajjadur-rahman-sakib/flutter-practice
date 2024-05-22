22902979ef40b3c5aeab05417b4dfdfc0dc64514
sakib <sajjadur.rahman.sakib.x@gmail.com>
2024-05-22 23:57:43 +0600

    Commit

---
+++ module_12_assignment/lib/main.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+import 'package:module_12_assignment/view/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ module_12_assignment/lib/view/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'home screen/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Photo Gallery',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
