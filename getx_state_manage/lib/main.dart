7a6c2e42664f68b44b1660777e5f061d07b7470c
Sajjadur Rahman Sakib <102736598+sajjadur-rahman-sakib@users.noreply.github.com>
2024-07-14 23:50:14 +0600

    Commit

--- getx_state_manage/lib/main.dart
+++ getx_state_manage/lib/main.dart
@@ -11,23 +11,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
+    return GetMaterialApp(
+      home: const Home(),
+      initialBinding: ControllerBindings(),
     );
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
-
+class ControllerBindings extends Bindings {
   @override
-  State<Home> createState() => _HomeState();
+  void dependencies() {
+    Get.put(CounterController());
+  }
+
 }
 
-class _HomeState extends State<Home> {
-  // final CounterController counterController = CounterController();
-  final CounterController counterController = Get.put(CounterController());
+class Home extends StatelessWidget {
+  const Home({super.key});
 
+  // final CounterController counterController = CounterController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,14 +49,69 @@ class _HomeState extends State<Home> {
               return Text(
                 "Total Count : ${counterController.count}",
               );
-            })
+            }),
+            TextButton(onPressed: (){
+              Get.to(const Profile());
+            }, child: const Text("Profile")),
+            TextButton(onPressed: (){
+              Get.to(const HomePage());
+            }, child: const Text("Home")),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: counterController.incrementCount,
+        onPressed: Get.find<CounterController>().incrementCount,
         child: const Icon(Icons.add),
       ),
     );
   }
 }
+
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: (){
+            Get.back();
+          }, child: const Text("Back")),
+          ElevatedButton(onPressed: (){
+            Get.off(const HomePage());
+          }, child: const Text("Home")),
+        ],
+      ),
+    );
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: (){
+            Get.offAll(const Home());
+          }, child: const Text("Back to center")),
+          ElevatedButton(onPressed: (){
+            Get.to(const Profile());
+          }, child: const Text("Profile")),
+        ],
+      ),
+    );
+  }
+}
+
