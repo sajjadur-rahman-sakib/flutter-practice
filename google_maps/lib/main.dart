387f389958a9a8347cc4297bfb556933389c4ba3
sakib <sajjadur.rahman.sakib.x@gmail.com>
2024-08-15 22:24:17 +0600

    Commit

---
+++ google_maps/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Google Maps"),
+        centerTitle: true,
+      ),
+      body: const GoogleMap(
+          initialCameraPosition: CameraPosition(
+        target: LatLng(22.464374125579464, 90.34113520986755),
+      )),
+    );
+  }
+}
