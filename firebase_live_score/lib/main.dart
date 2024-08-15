47b9996c6e7aa10ff1ee9b9054d27d14c5f1e163
sakib <sajjadur.rahman.sakib.x@gmail.com>
2024-08-15 11:58:32 +0600

    Commit

--- firebase_live_score/lib/main.dart
+++ firebase_live_score/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase/firebase_notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -8,6 +9,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseNotificationService.instance.initialize();
   runApp(const MyApp());
 }
 
